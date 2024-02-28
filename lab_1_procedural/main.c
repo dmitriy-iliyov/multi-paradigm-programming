cabf96a467eeee14def6c99329ca9ead0cd13958
Dima Iliov <dmitriy.iliyov@gmail.com>
2024-02-29 01:42:31 +0200

    reformat code

--- lab_1_procedural/main.c
+++ lab_1_procedural/main.c
@@ -9,65 +9,54 @@
 #define SIZE 6
 #define FLAG 0
 
+int size;
+int * array;
+int * sorted_array;
+char alphabet[ALPHABET_POWER];
+char char_array[SIZE];
+double matrix_interval[ALPHABET_POWER][2];
+char result_matrix [ALPHABET_POWER + 1][ALPHABET_POWER + 1];
 
-int main (){
-    int size;
-    int * array;
-
-    if(FLAG == 1){
-        printf("Enter count of values: ");
-        scanf("%d", &size);
-        array = (int*)malloc(size * sizeof(int));
-        for (int i = 0; i < size; i++){
-            printf("a[%d] = ", i);
-            scanf("%d", &array[i]);
-        }
+void valuesByUser(){
+    printf("Enter count of values: ");
+    scanf("%d", &size);
+    array = (int*)malloc(size * sizeof(int));
+    for (int i = 0; i < size; i++){
+        printf("a[%d] = ", i);
+        scanf("%d", &array[i]);
     }
-    else{
-        size = SIZE;
-        array = (int*)malloc(size * sizeof(int));
-        srand(time(NULL));
-        for (int i = 0; i < size; i++) {
-            array[i] = rand() % 201 - 100;
-        }
+}
+
+void valuesByRandom(){
+    size = SIZE;
+    array = (int*)malloc(size * sizeof(int));
+    srand(time(NULL));
+    for (int i = 0; i < size; i++) {
+        array[i] = rand() % 201 - 100;
     }
+}
 
-    printf("default array:\n");
-    for (int i = 0; i < size; i++)
-        printf("%d ", array[i]);
-    printf("\n");
-
-    int *sorted_array = (int*)malloc(size * sizeof(int));;
-    for (int i = 0; i < SIZE; i++) {
+void sortArray(){
+    sorted_array = (int*)malloc(size * sizeof(int));
+    for (int i = 0; i < SIZE; i++)
         sorted_array[i] = array[i];
-    }
-
     for(int i = 1; i < size; i++){
-        for(int k=i; k>0 &&  sorted_array[k-1] > sorted_array[k]; k--){
+        for(int k = i; k > 0 &&  sorted_array[k-1] > sorted_array[k]; k--){
             int tmp = sorted_array[k-1];
             sorted_array[k-1] = sorted_array[k];
             sorted_array[k] = tmp;
         }
     }
+}
 
-    printf("sorted array:\n");
-    for (int i = 0; i < size; i++)
-        printf("%d ", sorted_array[i]);
-    printf("\n");
-
-    char alphabet[ALPHABET_POWER];
+void setAlphabet(){
     for (int i = 0; i < ALPHABET_POWER; ++i) {
         alphabet[i] = 'A' + i;
     }
+}
 
-    printf("alphabet:\n");
-    for (int i = 0; i < ALPHABET_POWER; ++i) {
-        printf("%c ", alphabet[i]);
-    }
-    printf("\n");
-
+void cutToIntervals(){
     double interval_width = (sorted_array[size - 1] - sorted_array[0]) / (ALPHABET_POWER - 1);
-    double matrix_interval[ALPHABET_POWER][2];
     double interval[2];
     for (int i = 0; i < ALPHABET_POWER; i++) {
         double a = sorted_array[0] + i * interval_width;
@@ -81,13 +70,14 @@ int main (){
 
     printf("intervals:\n");
     for (int i = 0; i < ALPHABET_POWER; i++) {
-        for (int j = 0; j < 2; j++) {
+        for (int j = 0; j < 2; j++)
             printf("%c %f ", alphabet[i], matrix_interval[i][j]);
-        }
         printf("\n");
     }
+    printf("\n");
+}
 
-    char char_array[size];
+void intArrayToCharArray(){
     for(int i = 0; i < size; i++){
         for (int j = 0; j < ALPHABET_POWER; j++) {
             for (int k = 1; k < 2; k++) {
@@ -97,15 +87,9 @@ int main (){
             }
         }
     }
+}
 
-    printf("char array:\n");
-    for (int i = 0; i < size; ++i) {
-        printf("%c ", char_array[i]);
-    }
-    printf("\n");
-    printf("\n");
-
-    char result_matrix [ALPHABET_POWER + 1][ALPHABET_POWER + 1];
+void makeResultMatrix(){
     for (int i = -1; i < ALPHABET_POWER; i++) {
         for (int j = -1; j < ALPHABET_POWER; j++) {
             if (i == -1) {
@@ -123,7 +107,9 @@ int main (){
             }
         }
     }
+}
 
+void printResultMatrix(){
     printf("result_matrix:\n");
     for (int i = -1; i < ALPHABET_POWER; i++) {
         for (int j = -1; j < ALPHABET_POWER; j++) {
@@ -131,7 +117,55 @@ int main (){
         }
         printf("\n");
     }
+    printf("\n");
+}
+
+void printIntArray(int * _array, int size){
+    for (int i = 0; i < size; i++)
+        printf("%d ", _array[i]);
+    printf("\n");
+    printf("\n");
+}
+
+void printCharArray(char * charArray, int size){
+    for (int i = 0; i < size; ++i)
+        printf("%c ", charArray[i]);
+    printf("\n");
+    printf("\n");
+}
+
+
+int main (){
+
+    if(FLAG == 1){
+        valuesByUser();
+    }
+    else{
+        valuesByRandom();
+    }
+
+    printf("default array:\n");
+    printIntArray(array, SIZE);
+
+    sortArray();
+
+    printf("sorted array:\n");
+    printIntArray(sorted_array, SIZE);
+
+    setAlphabet();
+    printf("alphabet:\n");
+    printCharArray(alphabet, ALPHABET_POWER);
+
+    cutToIntervals();
+
+    intArrayToCharArray();
+    printf("char array:\n");
+    printCharArray(char_array, SIZE);
 
     free(array);
+
+    makeResultMatrix();
+    printResultMatrix();
+
     return 0;
 }
