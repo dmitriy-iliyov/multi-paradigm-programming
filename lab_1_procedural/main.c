10835b134a3cf5dfd4e048c17149ea1cf18e2e1a
Dima Iliov <dmitriy.iliyov@gmail.com>
2024-02-28 18:27:38 +0200

    changes

--- lab_1_procedural/main.c
+++ lab_1_procedural/main.c
@@ -5,8 +5,8 @@
 #include <math.h>
 
 
-#define ALPHABET_POWER 10
-#define SIZE 10
+#define ALPHABET_POWER 4
+#define SIZE 6
 #define FLAG 0
 
 
@@ -75,13 +75,10 @@ int main (){
     }
     printf("\n");
 
-    double F_a, F_b;
-    double shift = 0.0;
-    double scale = 1.0;
-    double interval_width = (sorted_array[size - 1] - sorted_array[0] + 3) / ALPHABET_POWER;
+    double interval_width = (sorted_array[size - 1] - sorted_array[0]) / (ALPHABET_POWER - 1);
     double matrix_interval[ALPHABET_POWER][2];
     double interval[2];
-    for (int i = 0; i < ALPHABET_POWER; ++i) {
+    for (int i = 0; i < ALPHABET_POWER; i++) {
         double a = sorted_array[0] + i * interval_width;
         double b = sorted_array[0] + (i + 1) * interval_width;
         interval[0] = a;
@@ -90,7 +87,6 @@ int main (){
             matrix_interval[i][j] = interval[j];
         }
     }
-
 
     printf("intervals:\n");
     for (int i = 0; i < ALPHABET_POWER; i++) {
@@ -116,48 +112,35 @@ int main (){
         printf("%c ", char_array[i]);
     }
     printf("\n");
+    printf("\n");
 
-//    double F_a, a, b;
-//    double shift = 0.0;
-//    double scale = 1.0;
-//    if (array[0] <= shift) {
-//        F_a = 0.5 * exp((array[0] - shift) / scale);
-//    } else {
-//        F_a =  1 - 0.5 * exp(-(array[0] - shift) / scale);
-//    }
-//    double probability = 1.0 / ALPHABET_POWER;
-//    double matrix_interval[ALPHABET_POWER][2];
-//    double interval[2];
-//    for (int i = 0; i < ALPHABET_POWER; i++) {
-//        if (F_a <= 1) {
-//            a = scale * log(2 * F_a) + shift;
-//        } else {
-//            a = -scale * -log((1 - F_a) / 0.5) + shift;
-//        }
-//        double F_b = probability + F_a;
-//        if (F_b <= 1) {
-//            b = scale * log(2 * F_b) + shift;
-//        } else {
-//            b = -scale * -log((1 - F_b) / 0.5) + shift;
-//        }
-//        interval[0] = a;
-//        interval[1] = b;
-//        printf("Интервал [%.50lf, %f] [%.50lf, %f] : вероятность = %f\n", F_a, a, F_b, b,  probability);
-//        for (int j = 0; j < 2; j++) {
-//            matrix_interval[i][j] = interval[j];
-//        }
-//        a = 0.0;
-//        F_a = F_b;
-//        F_b = 0.0;
-//    }
-//
-//    printf("intervals:\n");
-//    for (int i = 0; i < ALPHABET_POWER; i++) {
-//        for (int j = 0; j < 2; j++) {
-//            printf("%f\t", matrix_interval[i][j]);
-//        }
-//        printf("\n");
-//    }
+    char result_matrix [ALPHABET_POWER + 1][ALPHABET_POWER + 1];
+    for (int i = -1; i < ALPHABET_POWER; i++) {
+        for (int j = -1; j < ALPHABET_POWER; j++) {
+            if (i == -1) {
+                if (j == -1)
+                    result_matrix[i][j] = ' ';
+                else{
+                    result_matrix[i][j] = alphabet[j];
+                }
+            } else {
+                if (j == -1)
+                    result_matrix[i][j] = alphabet[i];
+                else{
+                    result_matrix[i][j] = '0';
+
+                }
+            }
+        }
+    }
+
+    printf("result_matrix:\n");
+    for (int i = -1; i < ALPHABET_POWER; i++) {
+        for (int j = -1; j < ALPHABET_POWER; j++) {
+            printf("%c ", result_matrix[i][j]);
+        }
+        printf("\n");
+    }
 
     free(array);
     return 0;
