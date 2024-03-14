43fef122a61af9211784994ce0f910fdfc806e06
Dima Iliov <dmitriy.iliyov@gmail.com>
2024-03-14 05:36:10 +0200

    upd previous commmit

--- lab_2_functional/main_task/main_task_code.lisp
+++ lab_2_functional/main_task/main_task_code.lisp
@@ -2,11 +2,14 @@
   (loop repeat size collect (random 100)))
 
 (defun sort-list (userList)
-  (setq sortedList (sort (subseq userList 0 (length userList)) '<)))
+  (setq sortedList (sort (subseq userList 0) '<)))
 
-(defun main (size alphabet-power)
+(defun main (size alphabetPower)
   (setq startList (values-by-random size))
   (sort-list startList)
+  (print "start list")
+  (print startList)
+  (print "sorted list")
   (print sortedList))
 
 (main 10 4)
