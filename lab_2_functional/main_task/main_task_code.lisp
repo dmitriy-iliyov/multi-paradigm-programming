af0d69b5bd9d9cd0f768917f4ebb13beffbd403b
Dima Iliov <dmitriy.iliyov@gmail.com>
2024-03-14 05:18:13 +0200

    rand array, sort array

--- lab_2_functional/main_task/main_task_code.lisp
+++ lab_2_functional/main_task/main_task_code.lisp
@@ -1,5 +1,12 @@
-(defun values-by-random (length)
-  (loop repeat length collect (random 100)))
+(defun values-by-random (size)
+  (loop repeat size collect (random 100)))
 
-(sort (values-by-random 7) '<)
+(defun sort-list (userList)
+  (setq sortedList (sort (subseq userList 0 (length userList)) '<)))
 
+(defun main (size alphabet-power)
+  (setq startList (values-by-random size))
+  (sort-list startList)
+  (print sortedList))
+
+(main 10 4)
