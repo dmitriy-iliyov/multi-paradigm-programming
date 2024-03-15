5385e8d3bc8294828ea32286dc81666c9cd10633
Dima Iliov <dmitriy.iliyov@gmail.com>
2024-03-15 18:14:10 +0200

    lab 2 v1

--- lab_2_functional/main_task/main_task_code.lisp
+++ lab_2_functional/main_task/main_task_code.lisp
@@ -13,26 +13,55 @@
 
 (defun cut-to-intervals (sorted-array alphabet-power &optional (i 0) (matrix-interval '()))
   (if (= i alphabet-power)
-      matrix-interval
+      (reverse matrix-interval)
       (let* ((interval-width (/ (- (elt sorted-array (1- (length sorted-array))) (elt sorted-array 0)) (- alphabet-power 1)))
              (a (+ (elt sorted-array 0) (* i interval-width)))
              (b (+ (elt sorted-array 0) (* (1+ i) interval-width)))
              (new-interval (list a b)))
         (cut-to-intervals sorted-array alphabet-power (1+ i) (cons new-interval matrix-interval)))))
 
-(defun to-char-array (array matrix-interval alphabet)
-  (let ((char-array (make-array (length array))))
-    (dotimes (i (length array))
+(defun to-char-list (start-list intervals alphabet)
+  (let ((char-list (make-array (length start-list))))
+    (dotimes (i (length start-list))
       (dotimes (j (length alphabet))
-        (dotimes (k 2)
-          (when (and (>= (elt array i) (elt (elt matrix-interval j) (- k 1)))
-                     (<= (elt array i) (elt (elt matrix-interval j) k)))
-            (setf (elt char-array i) (elt alphabet j)))))))
-    char-array)
+        (let ((a (elt (elt intervals j) 0))
+              (b (elt (elt intervals j) 1)))
+          (when (and (<= a (elt start-list i) b))
+            (setf (elt char-list i) (elt alphabet j))))))
+    char-list))
 
-(defun find-index (a char-array)
-  (cl-position a char-array :test #'char=))
+(defun find-index (a char-list &optional (index 0))
+  (if (null char-list)
+      nil
+      (if (and (characterp a) (stringp (car char-list)) (string= a (car char-list)))
+          index
+          (find-index a (cdr char-list) (1+ index)))))
 
+(defun make-result-matrix (char-array alphabet)
+  (let* ((alphabet-power (length alphabet))
+         (result-matrix (make-array (list alphabet-power alphabet-power)
+                                    :initial-element 0)))
+    (dotimes (i (length char-array))
+      (let* ((current-char (aref char-array i))
+             (current-index (if current-char
+                                (find-index current-char alphabet)
+                                nil)))
+        (when current-index
+          (let ((next-char (if (< (1+ i) (length char-array))
+                                (aref char-array (1+ i))
+                                nil))
+                (next-index (if next-char
+                                (find-index next-char alphabet)
+                                nil)))
+            (when next-index
+              (incf (aref result-matrix current-index next-index)))))))
+    result-matrix))
+
+(defun print-matrix (matrix)
+  (dotimes (i (array-dimension matrix 0))
+    (dotimes (j (array-dimension matrix 1))
+      (format t "~4d " (aref matrix i j)))
+    (format t "~%")))
 
 (defun main (size alphabet-power)
   (let ((start-list (values-by-random size))
@@ -47,6 +76,12 @@
       (format t "~%")
       (let ((intervals (cut-to-intervals sorted-list alphabet-power)))
         (dolist (interval intervals)
-          (format t "Interval: [~f, ~f]~%" (first interval) (second interval)))))))
+          (format t "Interval: [~f, ~f]~%" (first interval) (second interval)))
+        (let ((char-list (to-char-list start-list intervals alphabet)))
+          (format t "~A~%" char-list)
+          (let ((result-matrix (make-result-matrix char-list alphabet)))
+            (print-matrix result-matrix)))))))
+        
+
 
 (main 10 4)
